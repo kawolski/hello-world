a57b42a73c5a546995ba9066e66e220772e9d90a
kawolski <prashantxrawat@gmail.com>
2023-09-25 10:44:26 +0530

    DEV

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const PORT = 8080;
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World!...... PROD');
+  res.send('Hello World!...... DEV');
 });
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
